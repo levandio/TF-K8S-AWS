0c7d2353e6465f6517dd77aa5cf1f42a658f23e0
levandio <l.morbedadze@gmail.com>
2023-10-17 11:14:19 +0400

    updated terraform variables

--- variables.tf
+++ variables.tf
@@ -1,21 +1,28 @@
-variable "region" {
-  default = "us-east-2"
+variable "aws_region" {
+  description = "AWS region to launch instances in"
+  default     = "us-east-2"
 }
-variable "ami" {
-    type = map(string)
-    default = {
-        master = "ami-024e6efaf93d85776"
-        worker = "ami-024e6efaf93d85776"
-    }
-}  
+
+variable "amazon_linux_ami" {
+  description = "AMI ID for Amazon Linux"
+  type        = map(string)
+  default     = {
+    master = "ami-024e6efaf93d85776"
+    worker = "ami-024e6efaf93d85776"
+  }
+}
+
 variable "instance_type" {
-    type = map(string)
-    default = {
-        master = "t2.micro"
-        worker = "t2.micro"
-    }
+  description = "EC2 instance type"
+  type        = map(string)
+  default     = {
+    master = "t2.micro"
+    worker = "t2.micro"
+  }
 }
+
 variable "worker_count" {
-    type = number
-    default = 2
+  description = "Number of worker instances"
+  type        = number
+  default     = 2
 }
